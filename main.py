c06190986a9e61ecbf05907d570c854093dffb76
F1ejm <147535168+F1ejm@users.noreply.github.com>
2026-07-14 21:17:41 +0200

    download yt audio to file

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from bs4 import BeautifulSoup
-import requests
+import yt_dlp
 
-page = requests.get("https://toscrape.com")
-soup = BeautifulSoup(page.text, "html.parser")
+link = input("link to YT video")
+file_path = input("path to save file")
+ydl_options = {
+    "format": "bestaudio", # best or worst for video 
+    "outtmpl": file_path+ "/%(title)s.%(ext)s"
 
-nwm = soup.findAll("div", class_="col-md-6")
+}
 
-print(nwm)
+with yt_dlp.YoutubeDL(ydl_options) as ydl:
+    ydl.download([link])
