373593a3aacb195b43bb0b1f11c9660fb0338800
F1ejm <147535168+F1ejm@users.noreply.github.com>
2026-07-17 00:37:45 +0200

    cenverter

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import yt_dlp
+import ffmpeg
 
 link = input("link to YT video")
-file_path = input("path to save file")
+save_path = input("path to save file")
 ydl_options = {
     "format": "bestaudio", # best or worst for video 
-    "outtmpl": file_path+ "/%(title)s.%(ext)s"
+    "outtmpl": save_path+ "/%(title)s.%(ext)s"
 
 }
 
 with yt_dlp.YoutubeDL(ydl_options) as ydl:
     ydl.download([link])
+
+
+
+def convert(file_path):
+    input_file = ffmpeg.input(file_path)
+    output_audio = ffmpeg.output(input_file, "D:\git PROJEKTY\scraping_PY")
+    ffmpeg.run(output_audio)
