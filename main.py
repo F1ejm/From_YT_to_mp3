e02aad2c3b91dfd50af3c016dd7c4d62a1f5725d
F1ejm <147535168+F1ejm@users.noreply.github.com>
2026-07-17 17:49:31 +0200

    find file

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import yt_dlp
 import ffmpeg
+import os
+from pathlib import Path
+
 
 link = input("link to YT video")
 save_path = input("path to save file")
@@ -13,8 +16,18 @@ with yt_dlp.YoutubeDL(ydl_options) as ydl:
     ydl.download([link])
 
 
+def find(start, name): #name can be *.webm for specific file type
+    find = []
+    for dir in Path(start).rglob(name):
+        find.append(str(dir))
+    return find
 
 def convert(file_path):
     input_file = ffmpeg.input(file_path)
     output_audio = ffmpeg.output(input_file, "D:\git PROJEKTY\scraping_PY")
     ffmpeg.run(output_audio)
+
+
+#print(dict(enumerate(os.scandir(save_path))))
+#print(find(save_path,"*.webm")) 
+
